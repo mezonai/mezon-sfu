a1ff54d84fb2916bb33ad5863119018254bab5dc
Nguyen Tran Nhan <nguyentran@ncc.asia>
2026-07-20 09:30:09 +0700

    add log to debug

--- src/net/io_uring.c
+++ src/net/io_uring.c
@@ -219,7 +219,8 @@ static void handle_recv_cqe(sfu_ring_t *r, struct io_uring_cqe *cqe, sfu_packet_
   char peer_ip[64];
   uint16_t peer_port;
   format_ring_peer_addr((const struct sockaddr *)name, peer_ip, &peer_port);
-  SFU_LOG_DEBUG("LOW-LEVEL RECV: io_uring read %d bytes from %s:%u on fd=%d (bid=%u)", cqe->res, peer_ip, peer_port, r->fd, bid);
+
+  SFU_LOG_INFO("LOW-LEVEL RECV: io_uring read %d bytes from %s:%u on fd=%d (bid=%u)", cqe->res, peer_ip, peer_port, r->fd, bid);
 
   if (o->flags & MSG_TRUNC) {
     SFU_LOG_WARN("datagram truncated from %s:%u (bid=%u), dropping", peer_ip, peer_port, bid);
@@ -291,6 +292,9 @@ unsigned sfu_ring_reap(sfu_ring_t *r, unsigned max_count, sfu_packet_pool_t *pp,
         }
       } else if (cqe->res < 0) {
         SFU_LOG_WARN("send_zc error on fd=%d: %s", r->fd, strerror(-cqe->res));
+        if (!(cqe->flags & IORING_CQE_F_NOTIF)) {
+          SFU_LOG_INFO("SEND_ZC completed res=%d", cqe->res);
+        }
       }
     }
   }
--- src/runtime/worker.c
+++ src/runtime/worker.c
@@ -68,8 +68,6 @@ void sfu_room_forward_packet(sfu_worker_t *w, sfu_packet_t *pkt) {
 
   sfu_room_touch_peer(active_room, &pkt->peer_addr, pkt->peer_addr_len, w->worker_index);
 
-  SFU_LOG_INFO("room %" PRIu64 " peer_count=%u", active_room->room_id, active_room->peer_count);
-
   bool is_rtcp = sfu_rtp_is_rtcp(pkt->data, pkt->len);
   int plain_len = (int)pkt->len;
   bool unprotected =
@@ -82,8 +80,6 @@ void sfu_room_forward_packet(sfu_worker_t *w, sfu_packet_t *pkt) {
 
   sfu_peer_entry_t subs[SFU_ROOM_MAX_PEERS];
   uint32_t n = sfu_room_list_subscribers_excluding(active_room, &pkt->peer_addr, pkt->peer_addr_len, subs, SFU_ROOM_MAX_PEERS);
-
-  SFU_LOG_INFO("worker %u subscribers=%u", w->worker_index, n);
 
   for (uint32_t i = 0; i < n; i++) {
     sfu_peer_entry_t *sub = &subs[i];
@@ -112,8 +108,6 @@ void sfu_room_forward_packet(sfu_worker_t *w, sfu_packet_t *pkt) {
       continue;
     }
     enc->len = (uint32_t)enc_len;
-
-    SFU_LOG_INFO("worker fwd from %u to %u", w->worker_index, sub->worker_id);
 
     if (sub->worker_id == w->worker_index) {
       if (sfu_ring_queue_send_zc(&w->send_ring, enc, (const struct sockaddr *)&sub->addr, sub->addr_len) != 0) {
