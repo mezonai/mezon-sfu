8b1faa94956c5c43a5ed5f995992909d3eb7e7e5
Nguyen Tran Nhan <nguyentran@ncc.asia>
2026-07-29 14:12:21 +0700

    change keyed by address to ufrag

--- src/peer/session.h
+++ src/peer/session.h
@@ -12,6 +12,7 @@ sfu_peer_session_t *sfu_session_table_get_or_create(sfu_session_table_t *t, cons
 sfu_peer_session_t *sfu_session_table_find(sfu_session_table_t *t, const struct sockaddr_storage *addr, socklen_t addr_len);
 sfu_peer_session_t *sfu_session_table_find_by_ufrag(sfu_session_table_t *t, const char *ufrag);
 void sfu_session_table_remove(sfu_session_table_t *t, sfu_peer_session_t *s);
+void sfu_session_table_rebind_addr(sfu_session_table_t *t, sfu_peer_session_t *s, const struct sockaddr_storage *addr, socklen_t addr_len);
 
 static inline uint8_t sfu_session_get_mapped_pt(const sfu_peer_session_t *session, uint8_t incoming_pt) {
   /* Mask to 7 bits just in case, ensuring we never cause an out-of-bounds read */
